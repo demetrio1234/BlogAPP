da3ce12e4d197722ca3c6b96df67774170ba1090
DESKTOP-MJ4G07Q\demia <dem.iaria@gmail.com>
2023-10-08 11:28:44 +0100

    API:EFCore.SQL EFCore.Tools and added Models

---
+++ API/BlogAPP.API/WebAPP.API/Models/Doamin/BlogPost.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPP.API.Models.Doamin
+{
+    public class BlogPost
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public required string Title { get; set; }
+
+        public required string ShortDescription { get; set; }
+
+        public required string Content { get; set; }
+
+        public required string FeaturedImageUrl { get; set; }
+
+        public required string UrlHandle { get; set; }
+
+        public required DateTime PublishedDate { get; set; }
+
+        public required string Author { get; set; }
+
+        public required bool IsVisible { get; set; }
+
+    }
+}
+
+
---
+++ API/BlogAPP.API/WebAPP.API/Models/Doamin/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPP.API.Models.Doamin
+{
+    public class Category
+    {
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string UrlHandle { get; set; }
+
+    }
+}
