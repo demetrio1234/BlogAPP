b91dc76840cf736f0574e86ef13af5df6990e840
DESKTOP-MJ4G07Q\demia <dem.iaria@gmail.com>
2023-10-12 19:44:14 +0100

    Adjusted the routing of the Add Category button

--- UI/BlogAPP.UI/src/app/app.module.ts
+++ UI/BlogAPP.UI/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { NavbarComponent } from './core/components/navbar/navbar.component';
 import { CategoryListComponent } from './features/category/category-list/category-list.component';
 import { AddCategoryComponent } from './features/category/add-category/add-category.component';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -18,7 +18,9 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, FormsModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- UI/BlogAPP.UI/src/app/features/category/add-category/add-category.component.ts
+++ UI/BlogAPP.UI/src/app/features/category/add-category/add-category.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-add-category',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-category.component.css']
 })
 export class AddCategoryComponent {
+
+  model: AddCategoryRequest;
+
+  constructor(private categoryService: CategoryService) {
+    this.model = {
+      name: '',
+      urlHandle: ''
+    }
+  }
+
+  onFormSubmit() {
+    this.categoryService.addCategory(this.model).subscribe({
+      next: (response) => {
+
+      },error(err) {
+          
+      },
+    })
+  }
 
 }
