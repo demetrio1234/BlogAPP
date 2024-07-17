bb50ee01e8034162ef2352a068ca5786d6a8df56
demetrio <dem.iaria@gmail.com>
2024-07-17 23:46:28 +0200

    implement automapper and use it into user controller

---
+++ API/BlogAPP.API/WebAPP.API/Mapper/AutoMapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using WebAPP.API.Models.Domain;
+using WebAPP.API.Models.DTO.DTOs;
+
+
+namespace WebAPP.API.Mapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>().ReverseMap();
+        }
+    }
+}
