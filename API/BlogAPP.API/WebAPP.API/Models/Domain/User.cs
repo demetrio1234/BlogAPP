d36eb2d1ce506ce66dda9d2c98258aa47fcf3118
demetrio <dem.iaria@gmail.com>
2024-07-17 23:43:39 +0200

    set default values for the user's properties

--- API/BlogAPP.API/WebAPP.API/Models/Domain/User.cs
+++ API/BlogAPP.API/WebAPP.API/Models/Domain/User.cs
@@ -2,16 +2,16 @@
 {
     public class User
     {
-        public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string HashedPassword { get; set; }
-        public string Address { get; set; }
-        public string City { get; set; }
-        public string Region { get; set; }
-        public string PostalCode { get; set; }
-        public string Country { get; set; }
-        public string Phone { get; set; }
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string HashedPassword { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Region { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
 
     }
 }
