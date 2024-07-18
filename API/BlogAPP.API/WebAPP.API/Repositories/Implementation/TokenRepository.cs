f0fb6ad4d18e356c806c26c2c506bdf891d697e1
demetrio <dem.iaria@gmail.com>
2024-07-18 15:05:48 +0200

    refactor and modify validation by createjwttoken in tokenrepository

--- API/BlogAPP.API/WebAPP.API/Repositories/Implementation/TokenRepository.cs
+++ API/BlogAPP.API/WebAPP.API/Repositories/Implementation/TokenRepository.cs
@@ -12,32 +12,35 @@ namespace WebAPP.API.Repositories.Implementation
         private readonly IConfiguration configuration;
         public TokenRepository(IConfiguration configuration)
         {
-
             this.configuration = configuration;
         }
 
         public string CreateJwtToken(IdentityUser user, IList<string> roles)
         {
-            List<Claim> claims = new()
+            if (user != null && !string.IsNullOrEmpty(user.Email))
             {
-                new Claim (ClaimTypes.Email, user.Email),
-            };
+                List<Claim> claims = [
+                    new Claim(ClaimTypes.Email, user.Email),
+                ];
 
-            claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+                if (roles != null)
+                    claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
 
-            //JWT Security Token Parameters
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]));
-            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+                //JWT Security Token Parameters
+                SymmetricSecurityKey? key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(s: configuration["Jwt:Key"]));
+                SigningCredentials? credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-            JwtSecurityToken token = new(
-                issuer: configuration["Jwt:Issuer"],
-                audience: configuration["Jwt:Audience"],
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(15),
-                signingCredentials: credentials
-                );
+                JwtSecurityToken token = new(
+                    issuer: configuration["Jwt:Issuer"],
+                    audience: configuration["Jwt:Audience"],
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(15),
+                    signingCredentials: credentials
+                    );
 
-            return new JwtSecurityTokenHandler().WriteToken(token);
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+            return string.Empty;
         }
     }
 }
